7afcd4c7f9fa8616d28c2556a8bc4cc7a9195955
Mohammed <sajidcurious9@gmail.com>
2022-07-26 14:23:47 +0530

    first commit in dev

--- index.js
+++ index.js
@@ -14,23 +14,12 @@ let addTodo = (event) =>{
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
 
-    // creating a li inside the todoDiv
-    const newTodo = document.createElement('li');
-    newTodo.classList.add('todo-item');
-    newTodo.innerText = todoInput.value;
-    todoDiv.appendChild(newTodo);
-
-    // creating completed button
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fa-solid fa-check"';
-    completedButton.classList.add('complete-btn');
-    todoDiv.append(completedButton);
-
-      // creating delete button
-      const deleteButton = document.createElement('button');
-      deleteButton.innerHTML = '<i class="fa-solid fa-check"';
-      deleteButton.classList.add('delete-btn');
-      todoDiv.append(deleteButton);
+    const htmlData = `
+    <li id="newtodo"></li>
+    <button id="complete-btn"><i class="fa-solid fa-check"></i></button>
+    <button id="delete-btn"><i class="fa-solid fa-trash-can"></i></button>`;
+  todoDiv.insertAdjacentHTML('afterbegin',htmlData);
+  todoList.appendChild(todoDiv);
 
    
 
